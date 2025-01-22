d7fbe9085f5e1f4ac5c880c1341e82ff60e8cfe0
Jakub-Wilk <wilkjakub64@gmail.com>
2025-01-22 23:54:13 +0100

    Minor formatting and logging improvements

--- main.py
+++ main.py
@@ -15,13 +15,15 @@ app.logger.setLevel(gunicorn_logger.level)
 def get_donor_info():
     raw_data = {}
     data = {"t0": [], "t1": [], "t2": [], "t3": []}
+
     with db.DB() as cur:
-        app.logger.info("Fetching perms")
+        app.logger.debug("Fetching perms")
         cur.execute(db.FETCH_PERMS)
         for uuid, permission in cur:
             tier = permission[-2:]
             raw_data[uuid] = tier
-        app.logger.info("Fetching names")
+
+        app.logger.debug("Fetching names")
         cur.execute(db.FETCH_NAMES)
         for name, uuid in cur:
             if uuid not in raw_data.keys():
@@ -29,20 +31,22 @@ def get_donor_info():
             tier = raw_data[uuid]
             data[tier].append(name)
             del raw_data[uuid]
+
         to_insert = {}
-        app.logger.info(f"Fetching usernames for new uuids {raw_data.keys()}")
+        app.logger.debug(f"Fetching usernames for new uuids {raw_data.keys()}")
         for uuid, tier in raw_data.items():
             response = requests.get("https://api.minetools.eu/uuid/" + uuid)
             if response.status_code != 200:
-                print(
+                app.logger.error(
                     f"Error fetching name for UUID {uuid}, code {response.status_code}"
                 )
                 continue
             name = response.json()["name"]
             data[tier].append(name)
             to_insert[uuid] = name
+
         for uuid, name in to_insert.items():
-            app.logger.info(f"Inserting {uuid}: {name}")
+            app.logger.debug(f"Inserting {uuid}: {name}")
             try:
                 cur.execute(db.INSERT_NAMES, (uuid, name))
             except MariaError as e:
